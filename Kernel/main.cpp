a7db09b4066cb48023f140fa005777dc0c26b2e0
Matthew Costa <ucosty@gmail.com>
2021-12-22 15:06:49 +0000

    Kernel: Get linear framebuffer address from the BootState block

--- Kernel/main.cpp
+++ Kernel/main.cpp
@@ -2,19 +2,12 @@
 // Copyright (c) 2021 Matthew Costa <ucosty@gmail.com>
 //
 // SPDX-License-Identifier: GPL-3.0-only
+#include <BootState.h>
 #include <LinearFramebuffer.h>
+#include <Types.h>
 
-
-
-// TODO: Set up paging and jump into 0xffffffff80000000 + kernel_stage2
-extern "C" [[noreturn]] __attribute__((ms_abi)) void kernel_main(uint64_t framebuffer_address) {
-    auto framebuffer = LinearFramebuffer(framebuffer_address, 1280, 1024);
-
-
-
-    for(;;) {}
-}
-
-// Jump into this after basic paging setup
-void kernel_stage2() {
+extern "C" [[noreturn]] __attribute__((ms_abi)) void kernel_main(BootState *boot_state) {
+    auto framebuffer = LinearFramebuffer(boot_state->kernel_address_space.framebuffer.virtual_base, 1280, 1024);
+    framebuffer.rect(50, 50, 100, 100, 0xff0000, true);
+    for (;;) {}
 }
