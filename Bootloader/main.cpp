07d61d4798fe778b60546970c56181d65547d304
Matthew Costa <ucosty@gmail.com>
2021-12-22 14:55:50 +0000

    Bootloader: Set up BootState block and initial paging for the kernel
    Set up the initial page tables mapping the kernel and associated
    memory blocks into the top - 2GiB memory block (mcmodel = kernel).
    Pass information about this paging setup through to the kernel using
    the BootState structure.

--- Bootloader/main.cpp
+++ Bootloader/main.cpp
@@ -2,13 +2,18 @@
 // Copyright (c) 2021 Matthew Costa <ucosty@gmail.com>
 //
 // SPDX-License-Identifier: GPL-3.0-only
+#include <BootState.h>
 #include <ConsleIO.h>
 #include <EFI/Efi.h>
 #include <EFI/EfiWrapper.h>
 #include <ELF.h>
+#include <PageStructures.h>
 #include <Try.h>
 #include <UnbufferedConsole.h>
 #include <cstring.h>
+
+constexpr uint64_t GUARD_PAGE = 1;
+constexpr uint64_t kernel_virtual_base = 0xffffffff80000000;
 
 EFI::Raw::SimpleTextOutputProtocol *console_out;
 extern "C" void putchar(char c) {
@@ -21,13 +26,58 @@ extern "C" void putchar(char c) {
     while (true) {};
 }
 
-#define ASSERT(expression)                                               \
-    ({                                                                   \
-        if (!(expression)) {                                             \
-            printf("Assertion failed at %s:%d\r\n", __FILE__, __LINE__); \
-            panic("Halting");                                            \
-        }                                                                \
-    })
+uint64_t divide_rounded_up(uint64_t value, uint64_t divisor) {
+    return (value + (divisor - 1)) / divisor;
+}
+
+/**
+ * Get the number of memory pages required to fit the number of bytes
+ * provided, rounded up to the nearest page.
+ * @param bytes to fit
+ * @return page count
+ */
+uint64_t bytes_to_pages(uint64_t bytes) {
+    return divide_rounded_up(bytes, 0x1000);
+}
+
+uint64_t next_virtual_mapping(VirtualMapping &last) {
+    return last.virtual_base + last.size + 0x1000;
+}
+
+uint64_t get_efi_preserved_page_count(EFI::MemoryMap &memory_map) {
+    uint64_t total = 0;
+    for (auto &descriptor: memory_map) {
+        switch (descriptor.type) {
+            case EFI::Raw::EFI_RUNTIME_SERVICES_DATA:
+            case EFI::Raw::EFI_RUNTIME_SERVICES_CODE:
+            case EFI::Raw::EFI_ACPI_RECLAIM_MEMORY:
+            case EFI::Raw::EFI_ACPI_MEMORY_NVS:
+            case EFI::Raw::EFI_PAL_CODE:
+                total += descriptor.number_of_pages;
+                break;
+        }
+    }
+    return total;
+}
+
+uint64_t get_pagetable_size(uint64_t pages) {
+    // For every 2MiB of pages, we need a Page Table
+    auto page_tables = divide_rounded_up(pages, 2 * MiB);
+
+    // For every 1GiB of pages, we need a Page Directory
+    auto page_directories = divide_rounded_up(pages, 1 * GiB);
+
+    // For every 512GiB of pages, we need a PDP
+    auto page_directory_pointers = divide_rounded_up(pages, 512 * GiB);
+
+    // We need an additional PDPT for the temporary identity-mapping of memory
+    page_directory_pointers++;
+
+    // We always need a single PML4 page
+    auto pml4 = 1;
+
+    return pml4 + page_directory_pointers + page_directories + page_tables;
+}
 
 Result<EFI::GraphicsOutputProtocol> init_graphics(EFI::BootServices *boot_services) {
     auto graphics_output = TRY(boot_services->locate_protocol<EFI::GraphicsOutputProtocol>());
@@ -35,10 +85,10 @@ Result<EFI::GraphicsOutputProtocol> init_graphics(EFI::BootServices *boot_servic
     auto info = TRY(graphics_output.query_mode(mode));
     for (auto i = 0u; i < graphics_output.mode()->max_mode; i++) {
         info = TRY(graphics_output.query_mode(i));
-        if (info->PixelFormat != EFI::Raw::PixelRedGreenBlueReserved8BitPerColor && info->PixelFormat != EFI::Raw::PixelBlueGreenRedReserved8BitPerColor) {
+        if (info->pixel_format != EFI::Raw::PixelRedGreenBlueReserved8BitPerColor && info->pixel_format != EFI::Raw::PixelBlueGreenRedReserved8BitPerColor) {
             continue;
         }
-        if (info->HorizontalResolution == 1280 && info->VerticalResolution == 1024) {
+        if (info->width == 1280 && info->height == 1024) {
             mode = i;
             break;
         }
@@ -80,17 +130,95 @@ Result<void> init(EFI::Raw::Handle image_handle, EFI::Raw::SystemTable *system_t
             auto program_header = &program_headers[i];
             if (program_header->p_type != PT_LOAD)
                 continue;
-            total_pages += (program_header->p_memsz + 0x1000 - 1) / 0x1000;
+            total_pages += bytes_to_pages(program_header->p_memsz);
         }
         return total_pages;
     };
 
-    auto total_pages = get_total_pages(elf_header, program_headers);
-    auto kernel_base_addr = TRY(boot_services.allocate_pages(EFI::Raw::AllocateType::EFI_ALLOCATE_ANY_PAGES, EFI::Raw::MemoryType::EFI_LOADER_CODE, total_pages));
+    auto kernel_pages = get_total_pages(elf_header, program_headers);
+
+    // Set aside space for the initial page tables
+
+    /**
+     * Memory Map
+     *
+     * 0x0   ------------------------------
+     *  .    |                            |
+     *  .    |                            |
+     *  .    |                            |
+     *  .    |                            |
+     *  .    |  Unalllocated              |
+     *  .    |                            |
+     *  .    |                            |
+     *  .    |                            |
+     *  .    |                            |
+     * -2GiB ------------------------------
+     *  .    |                            |
+     *  .    | Kernel                     |
+     *  .    |                            |
+     *       ------------------------------
+     *       + Guard Page                 +
+     *       ------------------------------
+     *  .    | Boot State Block           |
+     *       ------------------------------
+     *       + Guard Page                 +
+     *       ------------------------------
+     *  .    |                            |
+     *  .    | Initial Pages              |
+     *  .    |                            |
+     *       ------------------------------
+     *       + Guard Page                 +
+     *       ------------------------------
+     *  .    |                            |
+     *  .    | Memory Map                 |
+     *  .    |                            |
+     *       ------------------------------
+     *       + Guard Page                 +
+     *       ------------------------------
+     *  .    |                            |
+     *  .    | Framebuffer                |
+     *  .    |                            |
+     *       ------------------------------
+     *       + Guard Page                 +
+     *       ------------------------------
+     *  .    |                            |
+     *  .    |                            |
+     *  .    | Unallocated                |
+     *  .    |                            |
+     *  .    |                            |
+     *       ------------------------------
+     */
+    auto memory_map = TRY(boot_services.get_memory_map());
+    TRY(memory_map.sanity_check());
+
+    // Start with the kernel pages
+    auto pages = kernel_pages + GUARD_PAGE;
+
+    // Add the memory map
+    pages += bytes_to_pages(memory_map.m_size) + GUARD_PAGE;
+
+    // Add the framebuffer
+    pages += bytes_to_pages(gfx.mode()->framebuffer_size) + GUARD_PAGE;
+
+    // Add the EFI preserved memory
+    pages += get_efi_preserved_page_count(memory_map) + GUARD_PAGE;
+
+    // Add the initial page table pages
+    auto pagetable_size = get_pagetable_size(pages);
+    pages += pagetable_size + GUARD_PAGE;
+
+    // Allocate memory for the kernel
+    auto kernel_physical_base = TRY(boot_services.allocate_pages(EFI::Raw::AllocateType::EFI_ALLOCATE_ANY_PAGES, EFI::Raw::MemoryType::EFI_LOADER_CODE, pages));
+    memset((char *) kernel_physical_base, 0, pages * 0x1000);
+
+    // Allocate memory for the boot state block
+    auto boot_state_page_count = bytes_to_pages(sizeof(BootState));
+    auto boot_state_base = TRY(boot_services.allocate_pages(EFI::Raw::AllocateType::EFI_ALLOCATE_ANY_PAGES, EFI::Raw::MemoryType::EFI_LOADER_CODE, boot_state_page_count));
+    memset((char *) boot_state_base, 0, boot_state_page_count * 0x1000);
+    auto boot_state = reinterpret_cast<BootState *>(boot_state_base);
 
     // Convert virtual addresses to kernel physical addresses
     auto kernel_virtual_to_physical = [](uint64_t physical_base, uint64_t virtual_address) {
-        static const uint64_t kernel_virtual_base = 0xffffffff80000000ull;
         return physical_base + (virtual_address - kernel_virtual_base);
     };
 
@@ -101,33 +229,121 @@ Result<void> init(EFI::Raw::Handle image_handle, EFI::Raw::SystemTable *system_t
             continue;
         }
 
-        auto load_pages = (program_header->p_memsz + 0x1000 - 1) / 0x1000;
-        total_pages += load_pages;
-
-        auto physical_address = kernel_virtual_to_physical(kernel_base_addr, program_header->p_vaddr);
+        auto load_pages = bytes_to_pages(program_header->p_memsz);
+        auto physical_address = kernel_virtual_to_physical(kernel_physical_base, program_header->p_vaddr);
         auto load_address = (void *) ((char *) kernel_bytes + program_header->p_offset);
         memset((char *) physical_address, 0, load_pages * 0x1000);
         memcpy((void *) physical_address, load_address, program_header->p_filesz);
     }
 
     // Get the system memory map
-    uint64_t memory_map_size = 0;
-    uint64_t memory_map_key = 0;
-    uint64_t memory_map_descriptor_size = 0;
-    uint32_t memory_map_descriptor_version = 0;
-    EFI::Raw::MemoryDescriptor *memory_descriptor;
-    boot_services.get_memory_map(&memory_map_size, nullptr, &memory_map_key, &memory_map_descriptor_size, &memory_map_descriptor_version);
-    ASSERT(memory_map_size != 0);
-    TRY(boot_services.allocate_pool(EFI::Raw::MemoryType::EFI_LOADER_DATA, memory_map_size, (void **) &memory_descriptor));
-    TRY(boot_services.get_memory_map(&memory_map_size, memory_descriptor, &memory_map_key, &memory_map_descriptor_size, &memory_map_descriptor_version));
+    memory_map = TRY(boot_services.get_memory_map());
 
     // Exit boot services mode
-    TRY(boot_services.exit_boot_services(image_handle, memory_map_key));
+    TRY(boot_services.exit_boot_services(image_handle, memory_map.m_key));
+
+    boot_state->framebuffer.base_address = gfx.mode()->framebuffer_base;
+    boot_state->framebuffer.size = gfx.mode()->framebuffer_size;
+    boot_state->framebuffer.width = gfx.mode()->info->width;
+    boot_state->framebuffer.height = gfx.mode()->info->height;
+
+    boot_state->kernel_address_space.base = 0xffffffff80000000;
+    boot_state->kernel_address_space.size = 2 * GiB;
+
+    // Kernel memory region
+    boot_state->kernel_address_space.kernel.virtual_base = 0xffffffff80000000;
+    boot_state->kernel_address_space.kernel.physical_base = kernel_physical_base;
+    boot_state->kernel_address_space.kernel.size = kernel_pages * Page;
+
+    // Boot state region
+    boot_state->kernel_address_space.boot_state.physical_base = boot_state_base;
+    boot_state->kernel_address_space.boot_state.virtual_base = next_virtual_mapping(boot_state->kernel_address_space.kernel);
+    boot_state->kernel_address_space.boot_state.size = boot_state_page_count * Page;
+
+    // Initial pages region
+    boot_state->kernel_address_space.initial_pages.physical_base = kernel_physical_base + (kernel_pages * Page);
+    boot_state->kernel_address_space.initial_pages.virtual_base = next_virtual_mapping(boot_state->kernel_address_space.boot_state);
+    boot_state->kernel_address_space.initial_pages.size = pagetable_size * Page;
+
+    // Memory map region
+    boot_state->kernel_address_space.memory_map.virtual_base = next_virtual_mapping(boot_state->kernel_address_space.initial_pages);
+    boot_state->kernel_address_space.memory_map.size = bytes_to_pages(memory_map.m_size) * Page;
+    boot_state->kernel_address_space.memory_map.physical_base = (uint64_t) memory_map.m_descriptors;
+
+    // Framebuffer region
+    boot_state->kernel_address_space.framebuffer.virtual_base = next_virtual_mapping(boot_state->kernel_address_space.memory_map);
+    boot_state->kernel_address_space.framebuffer.size = bytes_to_pages(gfx.mode()->framebuffer_size) * Page;
+    boot_state->kernel_address_space.framebuffer.physical_base = gfx.mode()->framebuffer_base;
+
+    // Set up initial paging
+    auto paging_physical_base = boot_state->kernel_address_space.initial_pages.physical_base;
+    auto pml4 = (PML4Entry *) paging_physical_base;                                       // 256 TiB / 512 GiB per entry
+    auto pdpt = (PageDirectoryPointerTableEntry *) paging_physical_base + 0x1000;         // 512 GiB / 1 GiB per entry
+    auto pdpt_identity = (PageDirectoryPointerTableEntry *) paging_physical_base + 0x2000;// 512 GiB / 1 GiB per entry
+    auto kernel_page_directory = (PageDirectoryEntry *) paging_physical_base + 0x3000;    // 1 GiB / 2 MiB per entry.
+    auto kernel_page_tables = (PageTableEntry *) paging_physical_base + 0x4000;           // 2 MiB / 4 KiB per entry
+
+    pml4[511].present = 1;
+    pml4[511].writeable = 1;
+    pml4[511].physical_address = ((uint64_t) pdpt) >> 12;
+
+    // Temporarily identity-map the first 512 GiB of memory
+    pml4[0].present = 1;
+    pml4[0].writeable = 1;
+    pml4[0].physical_address = ((uint64_t) pdpt_identity) >> 12;
+    for (int i = 0; i < 512; i++) {
+        pdpt_identity[i].present = 1;
+        pdpt_identity[i].writeable = 1;
+        pdpt_identity[i].size = 1;
+        pdpt_identity[i].physical_address = (i * GiB) >> 12;
+    }
+
+    pdpt[510].present = 1;
+    pdpt[510].writeable = 1;
+    pdpt[510].physical_address = ((uint64_t) kernel_page_directory) >> 12;
+
+    auto page_tables_count = divide_rounded_up(pages, 2 * MiB);
+    for (int i = 0; i < page_tables_count; i++) {
+        auto page_table_address = ((uint64_t) kernel_page_tables) + (i * 0x1000);
+        kernel_page_directory[i].present = 1;
+        kernel_page_directory[i].writeable = 1;
+        kernel_page_directory[i].physical_address = page_table_address >> 12;
+    }
+
+    auto virtual_to_page_table = [](uint64_t virtual_address) -> uint64_t {
+        auto offset_address = virtual_address - kernel_virtual_base;
+        return offset_address >> 12;
+    };
+
+    auto map_address_space = [&virtual_to_page_table, &kernel_page_tables](VirtualMapping &address_space) {
+        auto pages = address_space.size / 0x1000;
+        for (auto i = 0ull; i < pages; i++) {
+            auto page_physical = address_space.physical_base + (i * 0x1000);
+            auto page_virtual = address_space.virtual_base + (i * 0x1000);
+            auto page_table_entry = virtual_to_page_table(page_virtual);
+            kernel_page_tables[page_table_entry].present = 1;
+            kernel_page_tables[page_table_entry].writeable = 1;
+            kernel_page_tables[page_table_entry].physical_address = page_physical >> 12;
+        }
+    };
+
+    // Map the kernel
+    map_address_space(boot_state->kernel_address_space.kernel);
+    map_address_space(boot_state->kernel_address_space.boot_state);
+    map_address_space(boot_state->kernel_address_space.initial_pages);
+    map_address_space(boot_state->kernel_address_space.memory_map);
+    map_address_space(boot_state->kernel_address_space.framebuffer);
+
+    // Disable interrupts and load the new page tables
+    asm volatile("cli");
+    asm volatile("mov %%rax, %%cr3"
+                 : /* no output */
+                 : "a"(paging_physical_base)
+                 : "memory");
 
     // Jump into the kernel main function
-    auto entrypoint = kernel_virtual_to_physical(kernel_base_addr, elf_header->e_entry);
-    auto kernel_main = reinterpret_cast<void (*)(uint64_t)>(entrypoint);
-    kernel_main(gfx.mode()->framebuffer_base);
+    auto kernel_main = reinterpret_cast<void (*)(uint64_t)>(elf_header->e_entry);
+    kernel_main(boot_state->kernel_address_space.boot_state.virtual_base);
     return {};
 }
 
