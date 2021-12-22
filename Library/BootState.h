1d6c45a760fb4ea7e15ab26860d513bbbc661344
Matthew Costa <ucosty@gmail.com>
2021-12-22 14:41:43 +0000

    Library: Add BootState struct for kernel startup information
    The BootState struct is initialised by the bootloader and forwarded
    on to the kernel, and is used to pass information from the boot phase
    to the kernel.

---
+++ Library/BootState.h
@@ -0,0 +1,35 @@
+// WorkbenchOS
+// Copyright (c) 2021 Matthew Costa <ucosty@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-only
+#pragma once
+
+#include <Types.h>
+
+struct Framebuffer {
+    uint64_t base_address;
+    size_t size;
+    uint64_t width, height;
+};
+
+struct VirtualMapping {
+    uint64_t physical_base;
+    uint64_t virtual_base;
+    size_t size;
+};
+
+struct KernelAddressSpace {
+    uint64_t base;
+    size_t size;
+
+    VirtualMapping kernel;
+    VirtualMapping boot_state;
+    VirtualMapping initial_pages;
+    VirtualMapping memory_map;
+    VirtualMapping framebuffer;
+};
+
+struct BootState {
+    Framebuffer framebuffer;
+    KernelAddressSpace kernel_address_space;
+};
