8a286be8333439f40cf4fc5699c3c199a3424efe
Matthew Costa <ucosty@gmail.com>
2021-12-23 15:03:21 +0000

    Kernel: Fix GDT setup and change of CS selector
    This patch fixes the generation of the GDT, and the code that jumps
    from the existing UEFI descriptors to the new kernel-defined code
    selector.

--- Kernel/main.cpp
+++ Kernel/main.cpp
@@ -7,55 +7,49 @@
 #include <LinearFramebuffer.h>
 #include <Types.h>
 
-alignas(8) DescriptorTablePointer gdt_pointer{};
-alignas(8) SegmentDescriptor segments[5] = {
-    // Null segment
-    {},
-    // Kernel Code Segment
-    {
-        .segment_type = SEGMENT_EXECUTE,
-        .descriptor_type = DESCRIPTOR_CODE_DATA,
-        .present = 1,
-        .code_segment_64bit = 1,
-    },
-    // Kernel Data Segment
-    {
-        .segment_type = SEGMENT_READ_WRITE,
-        .descriptor_type = DESCRIPTOR_CODE_DATA,
-        .present = 1,
-        .code_segment_64bit = 0,
-    },
-    // User Code Segment
-    {
-        .segment_type = SEGMENT_EXECUTE,
-        .descriptor_type = DESCRIPTOR_CODE_DATA,
-        .privilege_level = 3,
-        .present = 1,
-        .code_segment_64bit = 1,
-    },
-    // User Data Segment
-    {
-        .segment_type = SEGMENT_READ_WRITE,
-        .descriptor_type = DESCRIPTOR_CODE_DATA,
-        .privilege_level = 3,
-        .present = 1,
-        .code_segment_64bit = 0,
-    },
-};
-
-extern "C" [[noreturn]] __attribute__((ms_abi)) void kernel_main(BootState *boot_state) {
-    auto framebuffer = LinearFramebuffer(boot_state->kernel_address_space.framebuffer.virtual_base, 1280, 1024);
-
+extern "C" [[noreturn]] void kernel_stage2(const BootState &boot_state) {
+    // TODO: Set up exception handlers
     // TODO: Relocate the stack -- done
     // TODO: Set up new GDT and jump into new kernel code segment -- done
     // TODO: Set up global page tables for kernel and whole-memory access
     // TODO: Set up IDT and basic exception handlers
     // TODO: Early stage boot console
-    gdt_pointer.address = reinterpret_cast<uint64_t>(&segments);
-    gdt_pointer.limit = 5;
-    asm volatile("lgdt (gdt_pointer)");
-
+    auto framebuffer = LinearFramebuffer(boot_state.kernel_address_space.framebuffer.virtual_base, 1280, 1024);
     framebuffer.rect(50, 50, 100, 100, 0x4455aa, true);
 
-    for (;;) {}
+    while (true) {}
+}
+
+static constexpr size_t gdt_descriptors = 5;
+alignas(8) DescriptorTablePointer gdt_pointer{};
+alignas(8) SegmentDescriptor segments[gdt_descriptors] = {
+    // Null segment
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    // Kernel Code Segment
+    {0, 0, 0, SEGMENT_EXECUTE, 1, 0, 1, 0, 0, 1, 0, 0, 0},
+    // Kernel Data Segment
+    {0, 0, 0, SEGMENT_READ_WRITE, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+    // User Code Segment
+    {0, 0, 0, SEGMENT_EXECUTE, 1, 3, 1, 0, 0, 1, 0, 0, 0},
+    // User Data Segment
+    {0, 0, 0, SEGMENT_READ_WRITE, 1, 3, 1, 0, 0, 0, 0, 0, 0},
+};
+
+extern "C" [[noreturn]] __attribute__((ms_abi)) void kernel_main(uint64_t boot_state_address) {
+    gdt_pointer.address = reinterpret_cast<uint64_t>(&segments);
+    gdt_pointer.limit = sizeof(SegmentDescriptor) * gdt_descriptors;
+    asm volatile("lgdt gdt_pointer\n"
+                 "mov %%rax, %%rdi\n"// Boot state block
+                 "mov $0x10, %%rbx\n"
+                 "mov %%rbx, %%ss\n"
+                 "mov %%rbx, %%ds\n"
+                 "mov %%rbx, %%es\n"
+                 "mov %%rbx, %%fs\n"
+                 "mov %%rbx, %%gs\n"
+                 "pushq $0x08\n"         // Push CS
+                 "pushq $kernel_stage2\n"// Push RIP
+                 "lretq\n"
+                 : /* no output */
+                 : "a"(boot_state_address));
+    while (true) {}
 }
