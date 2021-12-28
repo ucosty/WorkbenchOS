7df27494c49ad3261dc0cb74bd74dd23a45f8467
Matthew Costa <ucosty@gmail.com>
2021-12-28 21:10:26 +0000

    Kernel: Add SLAB allocator
    The SLAB allocator allows the kernel to efficiently allocate small
    objects (less than one page in size) by carving them out of whole pages.

---
+++ Kernel/Heap/SlabAllocator.cpp
@@ -0,0 +1,106 @@
+// WorkbenchOS
+// Copyright (c) 2021 Matthew Costa <ucosty@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-only
+#include "SlabAllocator.h"
+#include "../Debugging.h"
+#include "../Memory/MemoryManager.h"
+#include <Try.h>
+
+namespace Kernel {
+constexpr size_t MAX_OBJECT_SIZE = Page - sizeof(SlabPage);
+Result<NonNullPtr<Slab>> SlabAllocator::get_or_create_slab(size_t size) {
+    VERIFY(size < MAX_OBJECT_SIZE);
+    auto slab = find_slab(size);
+    if (slab.is_error()) {
+        return create_slab(size);
+    }
+    return Lib::Error::from_code(1);
+}
+
+Result<NonNullPtr<Slab>> SlabAllocator::find_slab(size_t size) {
+    for (auto &slab: m_slabs) {
+        if (slab.is_slab_for(size)) {
+            return NonNullPtr<Slab>::from(&slab);
+        }
+    }
+    return Lib::Error::from_code(1);
+}
+
+Result<NonNullPtr<Slab>> SlabAllocator::create_slab(size_t size) {
+    for (auto &slab: m_slabs) {
+        if (!slab.is_initialised()) {
+            TRY(slab.initialise(size));
+            return NonNullPtr<Slab>::from(&slab);
+        }
+    }
+    return Lib::Error::from_code(1);
+}
+
+Result<void> Slab::initialise(size_t size) {
+    auto &memory_manager = Kernel::MemoryManager::get_instance();
+    m_initialised = true;
+    m_object_size = size;
+    auto page_address = TRY(memory_manager.allocate_kernel_heap_page());
+    m_head = reinterpret_cast<SlabPage *>(page_address.as_ptr());
+    m_head->initialise(size);
+    return {};
+}
+
+Result<NonNullPtr<uint8_t>> Slab::allocate() {
+    VERIFY(m_head != nullptr);
+    for (auto page = m_head; page != nullptr; page = page->next()) {
+        if (page->has_free_objects()) {
+            auto object = TRY(page->allocate());
+            return NonNullPtr<uint8_t>::from(object);
+        }
+    }
+    return NonNullPtr<uint8_t>::from(reinterpret_cast<unsigned char *>(0xdeadc0de));
+}
+
+Result<void> Slab::free(VirtualAddress address) {
+    for (auto page = m_head; page != nullptr; page = page->next()) {
+        if (page->contains_object_at_address(address)) {
+            TRY(page->free(address));
+            return {};
+        }
+    }
+    return Lib::Error::from_code(1);
+}
+
+Result<void> SlabPage::initialise(size_t size) {
+    m_object_size = size;
+    m_total_objects = (Page - sizeof(SlabPage)) / size;
+    m_free_objects = m_total_objects;
+    for (int i = 0; i < m_total_objects; i++) {
+        auto object_address = get_object_address(i);
+        auto next_object_address = i < m_total_objects ? get_object_address(i + 1).as_address() : 0;
+        auto free_object = reinterpret_cast<FreeObject *>(object_address.as_address());
+        free_object->next = reinterpret_cast<FreeObject *>(next_object_address);
+    }
+    m_free_list = reinterpret_cast<FreeObject *>(get_object_address(0).as_address());
+    return {};
+}
+
+Result<uint8_t *> SlabPage::allocate() {
+    if (m_free_list == nullptr) {
+        return Lib::Error::from_code(1);
+    }
+    auto free_object = m_free_list;
+    m_free_list = free_object->next;
+    m_free_objects--;
+    return reinterpret_cast<uint8_t *>(free_object);
+}
+
+Result<void> SlabPage::free(VirtualAddress address) {
+    auto free_object = reinterpret_cast<FreeObject *>(address.as_address());
+    if (m_free_list == nullptr) {
+        m_free_list = free_object;
+        m_free_list->next = nullptr;
+    } else {
+        free_object->next = m_free_list;
+        m_free_list = free_object;
+    }
+    return {};
+}
+}// namespace Kernel
---
+++ Kernel/Heap/SlabAllocator.h
@@ -0,0 +1,80 @@
+// WorkbenchOS
+// Copyright (c) 2021 Matthew Costa <ucosty@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-only
+#pragma once
+
+#include "VirtualAddress.h"
+#include <NonNullPtr.h>
+#include <Result.h>
+#include <Types.h>
+
+namespace Kernel {
+
+struct FreeObject {
+    FreeObject *next;
+};
+
+static_assert(sizeof(FreeObject) == 8);
+
+// Stored at the end of the slab page
+// | < 0                                               0x1000 > |
+// +------------------------------------------------------------+
+// | Item | Item | Item | Item | Item | Item | Slack | Metadata |
+// +------------------------------------------------------------+
+class SlabPage {
+public:
+    Result<void> initialise(size_t size);
+    [[nodiscard]] SlabPage *next() const { return m_next_page; }
+    [[nodiscard]] SlabPage *previous() const { return m_previous_page; }
+    [[nodiscard]] bool has_next() const { return m_next_page != nullptr; }
+    [[nodiscard]] bool has_free_objects() const { return m_free_objects > 0; }
+    [[nodiscard]] size_t total_objects() const { return m_total_objects; }
+    [[nodiscard]] size_t free_objects() const { return m_free_objects; }
+    [[nodiscard]] VirtualAddress get_object_address(size_t index) const {
+        auto page_address = reinterpret_cast<size_t>(this);
+        return VirtualAddress(page_address + sizeof(SlabPage) + (m_object_size * index));
+    }
+    [[nodiscard]] Result<uint8_t *> allocate();
+    Result<void> free(VirtualAddress address);
+    [[nodiscard]] bool contains_object_at_address(VirtualAddress address) const {
+        auto base_address = reinterpret_cast<uint64_t>(this);
+        return (address.as_address() > base_address) &&
+            (address.as_address() < base_address + 0x1000);
+    }
+
+private:
+    size_t m_object_size;
+    size_t m_free_objects;
+    size_t m_total_objects;
+    SlabPage *m_next_page{nullptr};
+    SlabPage *m_previous_page{nullptr};
+    FreeObject *m_free_list{nullptr};
+};
+
+static_assert(sizeof(SlabPage) == 48);
+
+class Slab {
+public:
+    Result<void> initialise(size_t size);
+    [[nodiscard]] bool is_slab_for(size_t size) const { return m_initialised && m_object_size == size; }
+    [[nodiscard]] bool is_initialised() const { return m_initialised; }
+    [[nodiscard]] Result<NonNullPtr<uint8_t>> allocate();
+    [[nodiscard]] Result<void> free(VirtualAddress address);
+
+private:
+    SlabPage *m_head{nullptr};
+    bool m_initialised{false};
+    size_t m_object_size{0};
+};
+
+class SlabAllocator {
+public:
+    Result<NonNullPtr<Slab>> get_or_create_slab(size_t size);
+
+private:
+    Result<NonNullPtr<Slab>> find_slab(size_t size);
+    Result<NonNullPtr<Slab>> create_slab(size_t size);
+    Slab m_slabs[32];
+};
+}// namespace Kernel
