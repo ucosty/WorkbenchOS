587a01adf5c055d4c838fef66da3117d3a3a7f9b
Matthew Costa <ucosty@gmail.com>
2022-01-06 20:45:48 +0000

    Library: Add TSS descriptor and task structure

--- Library/Descriptors.h
+++ Library/Descriptors.h
@@ -33,6 +33,48 @@ struct SegmentDescriptor {
 };
 static_assert(sizeof(SegmentDescriptor) == 8);
 
+union TSSDescriptor {
+    struct {
+        uint64_t segment_limit : 16;
+        uint64_t base_low : 16;
+        uint64_t base_low_middle : 8;
+        uint64_t segment_type : 4;
+        uint64_t reserved : 1;
+        uint64_t privilege_level : 2;
+        uint64_t present : 1;
+        uint64_t segment_limit_high : 4;
+        uint64_t available : 1;
+        uint64_t reserved_1 : 2;
+        uint64_t granularity : 1;
+        uint64_t base_high_middle : 8;
+        uint32_t base_high;
+        uint32_t reserved_2;
+    };
+    struct {
+        uint64_t low;
+        uint64_t high;
+    };
+};
+static_assert(sizeof(TSSDescriptor) == 16);
+
+struct PACKED TSS {
+    uint32_t reserved;
+    uint64_t rsp0;
+    uint64_t rsp1;
+    uint64_t rsp2;
+    uint64_t reserved_2;
+    uint64_t ist1;
+    uint64_t ist2;
+    uint64_t ist3;
+    uint64_t ist4;
+    uint64_t ist5;
+    uint64_t ist6;
+    uint64_t ist7;
+    uint64_t reserved_3;
+    uint16_t reserved_4;
+    uint16_t iomap_base;
+};
+
 struct PACKED InterruptDescriptor {
     uint64_t offset : 16;
     uint64_t segment_selector : 16;
