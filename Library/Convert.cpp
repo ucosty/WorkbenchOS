1700784f136abc8f2e1c93f513fe86e706e0e729
Matthew Costa <ucosty@gmail.com>
2022-03-01 17:25:31 +0000

    Library: Add method to convert uint8_ts to strings

--- Library/Convert.cpp
+++ Library/Convert.cpp
@@ -7,6 +7,18 @@
 namespace Lib {
     static const char character_digits[] = "0123456789ABCDEF";
 
+    void uint8_t_to_cstring(uint8_t n, int base, int buffer_size, char *out) {
+        uint8_t output_index = 1;
+        uint8_t order_of_magnitude = 1;
+
+        do {
+            int digit = n % base;
+            out[buffer_size - output_index++] = character_digits[digit];
+            n /= base;
+            order_of_magnitude *= base;
+        } while (n > 0);
+    }
+
     void uint32_t_to_cstring(uint32_t n, int base, int buffer_size, char *out) {
         uint32_t output_index = 1;
         uint32_t order_of_magnitude = 1;
--- Library/Convert.h
+++ Library/Convert.h
@@ -5,6 +5,7 @@
 #include <Types.h>
 
 namespace Lib {
+    void uint8_t_to_cstring(uint8_t n, int base, int buffer_size, char *out);
     void int32_t_to_cstring(int32_t n, int base, int buffer_size, char *out);
     void uint32_t_to_cstring(uint32_t n, int base, int buffer_size, char *out);
     void uint64_t_to_cstring(uint64_t n, int base, int buffer_size, char *out);
