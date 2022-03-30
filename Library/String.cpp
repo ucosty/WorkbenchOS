170a8417890c64b119b3d10123f06324a2b311f8
Matthew Costa <ucosty@gmail.com>
2022-03-30 09:26:57 +0100

    Library: add copy and delete constructors for String objects

---
+++ Library/String.cpp
@@ -0,0 +1,19 @@
+// WorkbenchOS
+// Copyright (c) 2022 Matthew Costa <ucosty@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include <ConsoleIO.h>
+#include <String.h>
+
+namespace Lib {
+String::~String() {
+    delete[] m_characters;
+}
+
+String::String(const String &other) {
+    m_length = other.m_length;
+    m_characters = new char[other.m_length];
+    memcpy((void *) m_characters, other.m_characters, other.m_length);
+}
+}
--- Library/String.h
+++ Library/String.h
@@ -6,22 +6,29 @@
 
 #include <StringView.h>
 #include <Types.h>
+#include <Vector.h>
 
 namespace Lib {
 class String {
 public:
     String() = default;
 
+    ~String();
+
     explicit String(const char *characters) : m_characters(characters) {}
 
     constexpr String(const char *characters, size_t length) : m_characters(characters),
                                                               m_length(length) {}
+
+    String(const String &);
 
     constexpr explicit String(const StringView &sv) : m_characters(sv.m_characters), m_length(sv.m_length) {}
 
     [[nodiscard]] size_t length() const { return m_length; }
 
     [[nodiscard]] const char *as_cstring() const { return const_cast<char *>(m_characters); }
+
+    [[nodiscard]] inline constexpr char get(size_t index) const { return m_characters[index]; }
 
 private:
     const char *m_characters{nullptr};
