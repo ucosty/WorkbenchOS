f70c500b93488c9b6b96e9ce92b0368144d11a96
Matthew Costa <ucosty@gmail.com>
2022-12-15 21:55:48 +0000

    Add boot console

---
+++ Bootloader/BootConsole.cpp
@@ -0,0 +1,58 @@
+// WorkbenchOS
+// Copyright (c) 2022 Matthew Costa <ucosty@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include "BootConsole.h"
+#include "ConsoleFont.h"
+
+void BootConsole::write_character(char c) {
+    if(c == '\n') {
+        m_cursor_y += 16;
+        m_cursor_x = 0;
+        return;
+    }
+
+    auto glyph = font_basic[c];
+    for(int y = 0; y < 16; y++) {
+        auto row = glyph.row[y];
+        for(int x = 0; x < glyph.width; x++) {
+            if(row & (1 << x)) {
+                put_pixel(m_cursor_x + x, m_cursor_y + y, 0);
+            }
+        }
+    }
+
+    m_cursor_x += glyph.width;
+}
+
+void BootConsole::put_pixel(int x, int y, uint32_t colour) {
+    if(m_framebuffer == nullptr) return;
+
+    auto offset = x + (m_framebuffer_width * y);
+    m_framebuffer[offset] = colour;
+}
+
+void BootConsole::println(Std::StringView message) {
+    for(int i = 0; i < message.length(); i++) {
+        write_character(message.get(i));
+    }
+    m_cursor_y += 16;
+    m_cursor_x = 0;
+}
+
+void BootConsole::println(const char *message) {
+    while (*message != '\0') {
+        write_character(*message);
+        message++;
+    }
+    m_cursor_y += 16;
+    m_cursor_x = 0;
+}
+
+void BootConsole::print(const char *message) {
+    while (*message != '\0') {
+        write_character(*message);
+        message++;
+    }
+}
---
+++ Bootloader/BootConsole.h
@@ -0,0 +1,31 @@
+// WorkbenchOS
+// Copyright (c) 2022 Matthew Costa <ucosty@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+#include <LibStd/StringView.h>
+#include <LibStd/Types.h>
+
+class BootConsole {
+public:
+    void initialise(uint32_t *framebuffer, int framebuffer_width, int framebuffer_height) {
+        m_framebuffer = framebuffer;
+        m_framebuffer_width = framebuffer_width;
+        m_framebuffer_height = framebuffer_height;
+    }
+
+    void println(Std::StringView message);
+    void println(const char *message);
+    void print(const char *message);
+    void write_character(char c);
+    void put_pixel(int x, int y, uint32_t colour);
+
+private:
+    int m_cursor_x{0};
+    int m_cursor_y{0};
+    uint32_t *m_framebuffer{nullptr};
+    int m_framebuffer_width{0};
+    int m_framebuffer_height{0};
+};
